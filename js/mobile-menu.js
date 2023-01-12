2c8addc2d65aadd39c6baa60fe84c2ee9996278f
PavloMakarychak <117040148+PavloMakarychak@users.noreply.github.com>
2023-01-12 14:52:58 +0200

    modal franchise

---
+++ js/mobile-menu.js
@@ -0,0 +1,15 @@
+ 
+(() => {
+    const refs = {
+      openMenuRef: document.querySelector('.menu-open'),
+      closeMenuRef: document.querySelector('.mob-menu-close'),
+      menu: document.querySelector('.mob-menu'),
+    };
+  
+    refs.openMenuRef.addEventListener("click", toggleMenu);
+    refs.closeMenuRef.addEventListener("click", toggleMenu);
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle('is-open');
+    }
+  })();
